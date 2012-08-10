8239e2302edc4d4408494119efcb6eefd891dfb2
Filipe Giusti <filipegiusti@gmail.com>
2012-08-10 20:59:57 -0300

    Separate names LIVE.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 $(function () {
+  var $names_input = $('#names-input');
+  var $names_output = $('#names-output');
+
   var updateOutput = function () {
-    console.log($('#names-input').val());
+    // Separate some special characters
+    var separate_names = $names_input.val().split(/[.,:;\/\-=+\n]/);
+    // Remove empty names
+    separate_names = separate_names.filter(function (name) { return name.length > 0 });
+    // Remove whitespace and lowercase all names 
+    var clean_names = $.map(separate_names, function (name) { return name.trim().toLowerCase() });
+    // Uppercase all names
+    clean_names = $.map(clean_names, function (name) {
+      var name_parts = name.split(" ");
+      return $.map(name_parts, function (name_part) { return name_part.slice(0,1).toUpperCase() + name_part.slice(1) }).join(" ") ;
+    });
+
+    $names_output.val(clean_names.join("\n"));
   };
 
-  $('#names-input').change(updateOutput);
+  $names_input.bind('paste change keypress cut input', updateOutput);
 });
 
