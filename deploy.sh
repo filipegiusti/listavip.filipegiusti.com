138ae6798d7f1d2b887a395ea4a4c1901d9de470
Filipe Giusti <filipegiusti@gmail.com>
2013-02-15 17:50:24 -0200

    New host provider.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync -glpPrtvzC --delete . agiledhc@listavip.filipegiusti.com:~/public_html/listavip.filipegiusti.com
+rsync -glpPrtvzC --exclude=".gitignore" --exclude="deploy.sh" --exclude="README.md" --delete . filipegi@listavip.filipegiusti.com:~/public_html/sites/listavip.filipegiusti.com
 
