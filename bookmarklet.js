9f1b08e04d54050b242bd865e1c8d614145590eb
Filipe Giusti <filipegiusti@gmail.com>
2013-06-11 14:03:16 -0300

    Provide user some feedback before selecting.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,7 @@
 (function() {
   var selectAllFriends = function () {
+    alert('Pode levar alguns segundos até todos os amigos serem marcados, por favor aguarde.');
+
     var elems = document.getElementsByName('checkableitems[]');
     for(el in elems) {
       elems[el].click();
